9f41af1ea008f70710a9a067bea67e86b7f7e3cb
Jason Eccles <jasonxeccles@gmail.com>
2020-07-03 19:08:37 -0400

    Updates

---
+++ precache-manifest.7de852cec8d7fccf1b4db469eea6c8b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10986ca42438705ba8b87d5e1ba114c1",
+    "url": "/react-jamming-app/index.html"
+  },
+  {
+    "revision": "cf27b614d296446d5419",
+    "url": "/react-jamming-app/static/css/main.83580b25.chunk.css"
+  },
+  {
+    "revision": "0517651caa3c08151aac",
+    "url": "/react-jamming-app/static/js/2.dcb0f245.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-jamming-app/static/js/2.dcb0f245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf27b614d296446d5419",
+    "url": "/react-jamming-app/static/js/main.90e7ead5.chunk.js"
+  },
+  {
+    "revision": "c19605b21c4f40efa7c6",
+    "url": "/react-jamming-app/static/js/runtime-main.cedd144c.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/react-jamming-app/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
